2e741c4a0523a3fa4b54a8d275cbc7cbc53fdbe6
Arifinu <arifindriyanto0409@gmail.com>
2023-05-13 16:33:59 +0700

    Initial commit

---
+++ app/src/main/java/com/example/pertemuan7/SplashScreen.kt
@@ -0,0 +1,33 @@
+package com.example.pertemuan7
+
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import com.example.pertemuan7.databinding.ActivitySplashScreenBinding
+
+class SplashScreen : AppCompatActivity() {
+    lateinit var sharedPrefs : SharedPreferences
+    lateinit var binding : ActivitySplashScreenBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        sharedPrefs = getSharedPreferences("DataUser", Context.MODE_PRIVATE)
+
+        if (sharedPrefs.contains("user_id")){
+            val intent = Intent(this, DetailActivity::class.java)
+            startActivity(intent)
+            finish()
+        }else{
+            Handler().postDelayed({
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }, 2000)
+        }
+    }
+}
